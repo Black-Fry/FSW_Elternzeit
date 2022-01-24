58d7be027a89b4ff5479636ac4eeb95be54e24f3
Black-Fry <memento_mori666@web.de>
2022-01-24 21:17:39 +0100

    Update common.php

--- db_access/common.php
+++ db_access/common.php
@@ -1,12 +1,26 @@
 <?php
 
 /*  place your db-setup here    */
-
-
 $DB_HOST      = "";
 $DB_USERNAME  = "";
 $DB_PASSWORD  = "";
 $DB_NAME      = "";
+
+/*  SQL-Tab-Names    */	
+define ("T_FAM", "Tab_Familien");
+define ("T_EINSAETZE", "Tab_Einsaetze");
+
+
+/*  SQL-Tab-Field-Names    */	
+define ("F_FAM_ID", "FamID");
+define ("F_FAM_NAM", "FamNam");
+
+/*  HTML Table views    */
+define ("ADMIN_VIEW", "ADMIN");
+define ("USER_VIEW", "USER");
+
+/*  URL */
+define ("ROOT_URL", "http://fsw.ossoelmi.berlin");
 
 	
 ?>
